6a8bd7030f8ac1410ae538705c0731ae7d8dfc62
sauvageb <sauvageboris.pro@gmail.com>
2024-05-22 11:51:15 +0200

    filrouge : affichage du details

--- fil_rouge/src/main/java/com/training/demo/controller/TutorialController.java
+++ fil_rouge/src/main/java/com/training/demo/controller/TutorialController.java
@@ -5,11 +5,14 @@ import com.training.demo.entity.Tutorial;
 import com.training.demo.service.TutorialService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
+import java.util.Optional;
 
 
 @Controller
@@ -25,5 +28,16 @@ public class TutorialController {
         return new ModelAndView("tutorial-list", "tutorials", tutorialList);
     }
 
+    @GetMapping("/details/{idTutorial}")
+    public String displayTutorialDetails(@PathVariable("idTutorial") Long id, Model model) {
+        Optional<Tutorial> tutorialOpt = tutorialService.fetchTutorial(id);
+        if (tutorialOpt.isPresent()) {
+            Tutorial tuto = tutorialOpt.get();
+            model.addAttribute("tutorial", tuto);
+            return "tutorial-details";
+        } else {
+            return "redirect:/tutorials/list";
+        }
+    }
 
 }
--- fil_rouge/src/main/java/com/training/demo/service/TutorialService.java
+++ fil_rouge/src/main/java/com/training/demo/service/TutorialService.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -17,4 +18,7 @@ public class TutorialService {
         return (List<Tutorial>) tutorialRepository.findAll();
     }
 
+    public Optional<Tutorial> fetchTutorial(Long id) {
+        return tutorialRepository.findById(id);
+    }
 }
